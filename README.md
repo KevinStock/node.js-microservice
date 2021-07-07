1943941debe3b528ec43ebe28c0a2c158ed22207
Kevin Stock <kstock@uncomn.com>
2021-07-07 13:00:20 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Node.js Microservice Example
+
+## Install Dependencies
+### Node.js v16.x
+`curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -`
+`sudo apt-get install -y nodejs`
+
+### Mocha
+`npm install --save-dev mocha chai`
+`mkdir test`
+`touch test/test.js`
+
+## Setup
+`npm init`
+`npm install express request --save`
+
+## Execute Microservice
+`npm start`
+`npm test`
---
+++ test/test.js
@@ -0,0 +1,20 @@
+var expect = require('chai').expect;
+var request = require('request');
+
+describe('/GET distance', function() {
+    describe('Distance from 63109 to 63139', function() {
+        var url = "http://localhost:3000/distance/63109/63139"
+        it('returns status 200', function(done) {
+            request(url, function(error, response, body) {
+                expect(response.statusCode).to.equal(200);
+                done();
+            });
+        });
+        it ('returns the proper distane', function(done) {
+            request(url, function(error, response, body) {
+                expect(JSON.parse(body).distance).to.equal(1.792);
+                done();
+            })
+        });
+    });
+});
