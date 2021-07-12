df90bad43f5a373626f0c44123da150b38b34b76
Kevin Stock <kstock@uncomn.com>
2021-07-12 07:13:33 -0500

    move port to config file

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const config = require('./config/config.js');
+
 const app = express();
-const port = process.env.PORT || 3000;
+
 
 const routes = require('./api/routes');
 routes(app);
-app.listen(port, function() {
-    console.log('Server started on port: ' + port);
+app.listen(config.app.port, function() {
+    console.log('Server started on port: ' + config.app.port);
 });
